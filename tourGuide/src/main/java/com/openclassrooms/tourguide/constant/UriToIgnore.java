686fe053b99c5d79eabfb948ba3a3d8ca329ab96
Sébastien Cappon <contact.sae.echo@gmail.com>
2023-10-30 13:54:15 +0100

    Project downloaded and initialized with Eclipse and JUnit5. Actuators, Loggers and gitignore file added.

---
+++ tourGuide/src/main/java/com/openclassrooms/tourguide/constant/UriToIgnore.java
@@ -0,0 +1,22 @@
+package com.openclassrooms.tourguide.constant;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Constants class that regroups a short list of URI that the logger should
+ * ignore. <code>/error</code> is essential in order not to log Spring's
+ * automatic masked calls to the <code>/error</code> page when an error is
+ * thrown.
+ *
+ * @author Sébastien Cappon
+ * @version 1.0
+ */
+public class UriToIgnore {
+	public static final String errorURI = "/error";
+
+	public static final List<String> uriToIgnore = new ArrayList<>(Arrays.asList(
+			errorURI
+		));
+}
