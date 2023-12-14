ca501601f7b8a304fce0c250bf0d0ea9982c59fe
Sébastien Cappon <contact.sae.echo@gmail.com>
2023-12-14 14:16:07 +0100

    Documentation completed. Performances tests refactored.

--- tourGuide/src/test/java/com/openclassrooms/tourguide/performance/PerformanceTest.java
+++ tourGuide/src/test/java/com/openclassrooms/tourguide/performance/PerformanceTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.Test;
 
 import com.openclassrooms.tourguide.constant.InternalUsersQuantity;
 import com.openclassrooms.tourguide.model.User;
+import com.openclassrooms.tourguide.service.IRewardService;
+import com.openclassrooms.tourguide.service.ITourGuideService;
 import com.openclassrooms.tourguide.service.RewardService;
 import com.openclassrooms.tourguide.service.TourGuideService;
 
@@ -24,48 +26,48 @@ import rewardCentral.RewardCentral;
 public class PerformanceTest {
 
 	private GpsUtil gpsUtil = new GpsUtil();
-	private RewardService rewardsService = new RewardService(gpsUtil, new RewardCentral());
+	private IRewardService iRewardsService = new RewardService(gpsUtil, new RewardCentral());
 	private StopWatch stopWatch = new StopWatch();
 
 	@BeforeEach
 	public void setupTests() {
-		InternalUsersQuantity.setInternalUserQuantity(100000);
+		InternalUsersQuantity.setInternalUserQuantity(100);
 	}
 
 	@Test
 	public void highVolumeTrackLocation() throws InterruptedException {
-		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
+		ITourGuideService iTourGuideService = new TourGuideService(gpsUtil, iRewardsService);
 
 		List<User> allUsers = new ArrayList<>();
-		allUsers = tourGuideService.getUserList();
+		allUsers = iTourGuideService.getUserList();
 
 		stopWatch.start();
-			tourGuideService.trackEachUserLocation(allUsers);
+			iTourGuideService.trackEachUserLocation(allUsers);
 		stopWatch.stop();
 
-		tourGuideService.tracker.stopTracking();
+		iTourGuideService.getTracker().stopTracking();
 		assertTrue(TimeUnit.MINUTES.toSeconds(15) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 		System.out.println("highVolumeTrackLocation: Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds.");
 	}
 
 	@Test
 	public void highVolumeGetRewards() throws InterruptedException {
-		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
+		ITourGuideService iTourGuideService = new TourGuideService(gpsUtil, iRewardsService);
 
 		stopWatch.start();
 			Attraction attraction = gpsUtil.getAttractions().get(0);
 			List<User> userList = new ArrayList<>();
 	
-			userList = tourGuideService.getUserList();
+			userList = iTourGuideService.getUserList();
 			userList.forEach(user -> user.addVisitedLocation(new VisitedLocation(user.getUserId(), attraction, new Date())));
-			userList = rewardsService.manageEachUserRewardList(userList);
+			userList = iRewardsService.manageEachUserRewardList(userList);
 	
 			for (User user : userList) {
 				assertTrue(user.getUserRewardList().size() > 0);
 			}
 		stopWatch.stop();
 
-		tourGuideService.tracker.stopTracking();
+		iTourGuideService.getTracker().stopTracking();
 		assertTrue(TimeUnit.MINUTES.toSeconds(20) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 		System.out.println("highVolumeGetRewards: Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds.");
 	}
